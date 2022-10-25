b9270781cd0d4f78e009593d272c7a092bfe1299
briaa01 <113952340+briaa01@users.noreply.github.com>
2022-10-24 19:56:00 -0500

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+document.getElementById("box2").addEventListener("click", change color);
+function changeColor() {
+    document.getElementById("box2").innerHtml ="CLICK";
+    document.getElementById("box2").style.backgroundColor = "orange";
+};
